90bc2e269e228c2af1f191cc224b4c2126139d6a
Aayush Dutt <aayushd100@gmail.com>
2018-06-30 05:20:17 +0530

    add: add fs and initialisePackage

--- index.js
+++ index.js
@@ -1,60 +1,15 @@
+var fs = require('fs');
 
-const mongoose = require('mongoose')
+const initialisePackage = (data) => {
+  fs.writeFile("./package_test.json", JSON.stringify(data, null, 2), function(err) {
+    if(err) {
+        return console.log(err);
+    }
 
-
-// Connect to db
-const db = mongoose.connect('mongodb://localhost:27017/customerscli')
-
-const Customer = require('./models/customer')
-// Add Customer
-const addCustomer = (customer) => {
-  Customer.create(customer, function(){
-    console.info('New Customer Added')
-    mongoose.connection.close()
-  })
-}
-
-const findCustomer = (name) => {
-  // Make case insensitive
-  const search = new RegExp(name, 'i') //'i' means case insensitive
-  
-  Customer.find({$or: [{firstname: search}, {lastname: search}]})
-  .then(customer =>{
-    console.info(customer)
-    console.info(`${customer.length} matches`)
-    mongoose.connection.close()
-  })
-}
-
-const updateCustomer = (_id, customer)=>{
-  Customer.update({_id}, customer)
-  .then(customer => {
-    console.info('Update Successful')
-    mongoose.connection.close()
-  })
-}
-
-const removeCustomer = (_id)=>{
-  Customer.remove({_id})
-  .then(() => {
-    console.info('Removed!')
-    mongoose.connection.close()
-  })
-}
-
-const listCustomers = ()=>{
-  Customer.find()
-  .then((customers) => {
-    console.info(customers)
-    console.info(`${customers.length} customers`)
-    mongoose.connection.close()
-  })
+    console.log("package.json file created.");
+  }); 
 }
 
 module.exports = {
-  addCustomer,
-  findCustomer,
-  updateCustomer,
-  removeCustomer,
-  listCustomers
+  initialisePackage
 }
